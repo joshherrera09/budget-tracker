a56df8a83515d3e1f9c0fa2f2c8f86cafad2ba9d
Josh Herrera <joshherrera09@gmail.com>
2020-11-06 15:32:07 -0800

    service worker added

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/db.js',
+    '/styles.css'
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
