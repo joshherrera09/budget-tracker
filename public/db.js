5ac76a592a72cbe825d1cc3fb9bc49abe25e01b1
Josh Herrera <joshherrera09@gmail.com>
2020-11-06 15:09:21 -0800

    added onsuccess, onerror, saveRecord checkDatabase to db.js

--- public/db.js
+++ public/db.js
@@ -1,3 +1,51 @@
 var db;
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoincrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.taget.result;
+
+    if(navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    // Log error
+    console.log("Error" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/ransaction/bulk", {
+                mehod: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-type": "application/json"
+                }
+            }).then(response => response.json()).then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            })
+        }
+    }
+}
