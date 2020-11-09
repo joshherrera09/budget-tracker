6607ada5ae03415d4ff2a440c51d30b215d9c570
Josh Herrera <joshherrera09@gmail.com>
2020-11-09 12:01:08 -0800

    minor connection fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/glacier-beyond-53182',
+  process.env.MONGODB_URI || 'mongodb://localhost/populate',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
